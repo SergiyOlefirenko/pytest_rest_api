c42c2686d0d60ccf8f311cbd2513bde16d3c2105
Serhii Olefirenko <sergiy.olefirenko@gmail.com>
2023-02-14 13:40:24 +0200

    parametrized test and markers are added

--- tests/users/test_gorest_users.py
+++ tests/users/test_gorest_users.py
@@ -1,3 +1,4 @@
+import pytest
 from src.baseclasses.response import Response
 from src.pydantic_schemas.user import User
 
@@ -9,6 +10,20 @@ def test_get_users_list(get_users, calculate):
     print(calculate(1, 4))
     # <-- fixture that returned function
 
+@pytest.mark.production
+@pytest.mark.development
+@pytest.mark.skip('dummy test')
 def test_number(make_number):
     assert 1 == 1
     print(make_number)
+
+@pytest.mark.development
+@pytest.mark.parametrize('first_value, second_value, result', [
+    (1, 2, 3),
+    (-1, -2, -3),
+    (-1, 2, 1),
+    ('b', 1, None),
+    ('a', 'b', None)
+])
+def test_calculation(first_value, second_value, result, calculate):
+    assert calculate(first_value, second_value) == result
